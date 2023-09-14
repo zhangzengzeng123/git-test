521bf40d29de3519e2b4926187b5e5e8d1b4a99a
zhangzz <zzzjyjp2000@163.com>
2023-09-14 10:37:17 +0800

    master conflict commit

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("github test");
         System.out.println("hot-fix test merge");
+        System.out.println("master test");
     }
 }
